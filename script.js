21d9d20d572af382a140fb2d6fbd99c4c8abc51d
neelofar <neelo4khan@gmail.com>
2022-07-28 20:42:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,8 +23,11 @@ class GameOfLife {
 		this.colorBtns = document.querySelectorAll("[data-color]");
 		this.textInput = document.querySelector("#text");
 
+		this.speedRange = document.querySelector("#speed");
+		this.speedInput = this.speedRange.querySelector("input");
 
-
+		this.gridBtn = document.querySelector("#grid");
+		this.modeBtn = document.querySelector("#mode");
 
 		this.currentGenCells = [];
 		this.nextGenCells = [];
@@ -34,7 +37,8 @@ class GameOfLife {
 
 		// Start App
 		this.createSetup();
-	
+		this.addEvents();
+		this.addWindowEvents();
 	}
 
 	/* SETUP GAME BASICS */
@@ -88,7 +92,12 @@ class GameOfLife {
 		}
 	}
 
-
+	enableStartAndResetBtns() {
+		if (this.evolveBtn.disabled) {
+			this.evolveBtn.disabled = false;
+			this.resetBtn.disabled = false;
+		}
+	}
 
 	disableStartAndResetBtns() {
 		if (!this.evolveBtn.disabled) {
@@ -129,16 +138,373 @@ class GameOfLife {
 		this.createTwoDArrays();
 		this.addToggleCellClick();
 	}
+	/* EVOLUTION GAME RULES */
+	getNeighborCount(row, col) {
+		let nCount = 0;
+		const nRow = Number(row);
+		const nCol = Number(col);
 
+		// Make sure we are not at the first row
+		if (nRow - 1 >= 0) {
+			// Check top neighbor
+			if (this.currentGenCells[nRow - 1][nCol] === 1) {
+				nCount++;
+			}
+		}
 
+		// Make sure we are not in the first cell
+		// Upper left corner
+		if (nRow - 1 >= 0 && nCol - 1 >= 0) {
+			// Check upper left neighbor
+			if (this.currentGenCells[nRow - 1][nCol - 1] === 1) {
+				nCount++;
+			}
+		}
 
+		// Make sure we are not on the first row last column
+		// Upper right corner
+		if (nRow - 1 >= 0 && nCol + 1 < this.Cols) {
+			//Check upper right neighbor
+			if (this.currentGenCells[nRow - 1][nCol + 1] === 1) {
+				nCount++;
+			}
+		}
 
+		// Make sure we are not on the first column
+		if (nCol - 1 >= 0) {
+			// Check left neighbor
+			if (this.currentGenCells[nRow][nCol - 1] === 1) {
+				nCount++;
+			}
+		}
 
+		// Make sure we are not on the last column
+		if (nCol + 1 < this.Cols) {
+			// Check right neighbor
+			if (this.currentGenCells[nRow][nCol + 1] === 1) {
+				nCount++;
+			}
+		}
 
+		// Make sure we are not on the bottom left corner
+		if (nRow + 1 < this.Rows && nCol - 1 >= 0) {
+			// Check bottom left neighbor
+			if (this.currentGenCells[nRow + 1][nCol - 1] === 1) {
+				nCount++;
+			}
+		}
 
+		// Make sure we are not on the bottom right
+		if (nRow + 1 < this.Rows && nCol + 1 < this.Cols) {
+			// Check bottom right neighbor
+			if (this.currentGenCells[nRow + 1][nCol + 1] === 1) {
+				nCount++;
+			}
+		}
 
+		// Make sure we are not on the last row
+		if (nRow + 1 < this.Rows) {
+			// Check bottom neighbor
+			if (this.currentGenCells[nRow + 1][nCol] === 1) {
+				nCount++;
+			}
+		}
 
+		return nCount;
+	}
 
+	createNextGenerationArr() {
+		for (const row in this.currentGenCells) {
+			for (const col in this.currentGenCells[row]) {
+				let neighbors = this.getNeighborCount(row, col);
+
+				if (this.currentGenCells[row][col] === 1) {
+					// Alice Cells
+					if (neighbors < 2) {
+						this.nextGenCells[row][col] = 0;
+					} else if (neighbors === 2 || neighbors === 3) {
+						this.nextGenCells[row][col] = 1;
+					} else if (neighbors > 3) {
+						this.nextGenCells[row][col] = 0;
+					}
+				} else if (this.currentGenCells[row][col] === 0) {
+					//  Dead Cells
+					if (neighbors === 3) {
+						this.nextGenCells[row][col] = 1;
+					}
+				}
+			}
+		}
+	}
+
+	updateCurrGenerationArr() {
+		for (const row in this.currentGenCells) {
+			for (const col in this.currentGenCells[row]) {
+				this.currentGenCells[row][col] = this.nextGenCells[row][col];
+				this.nextGenCells[row][col] = 0;
+			}
+		}
+	}
+
+	updateCellsStatus() {
+		for (const row in this.currentGenCells) {
+			for (const col in this.currentGenCells[row]) {
+				const cell = document.getElementById(`${row}_${col}`);
+				if (cell) {
+					cell.className =
+						this.currentGenCells[row][col] === 0 ? this.dead : this.alive;
+				}
+			}
+		}
+	}
+	/* SETUP EVOLUTION EVENTS */
+	evolveCells() {
+		if (this.Cells) {
+			this.createNextGenerationArr();
+			this.updateCurrGenerationArr();
+			this.updateCellsStatus();
+		}
+
+		if (this.EVOLVING)
+			this.TIMER_ID = setTimeout(() => this.evolveCells(), this.timerSpeed);
+	}
+
+	startEvolve() {
+		this.EVOLVING = true;
+		this.evolveBtn.innerText = "Stop";
+		this.evolveBtn.title = "Pause alive cells evolution";
+		this.evolveCells();
+
+		if (!this.createBtn.disabled) this.createBtn.disabled = true;
+		if (this.speedInput.disabled) this.speedInput.disabled = false;
+	}
+
+	stopEvolve() {
+		this.EVOLVING = false;
+		this.evolveBtn.innerText = "Evolve";
+		this.evolveBtn.title = "Start alive cells evolution";
+		clearTimeout(this.TIMER_ID);
+	}
+
+	evolveBtnClick() {
+		// Toggle the evolve/stop of cell generation
+		if (!this.EVOLVING) {
+			this.startEvolve();
+			return;
+		}
+
+		this.stopEvolve();
+	}
+
+	resetBtnClick() {
+		this.intro.style.display = "block";
+
+		this.Cells.forEach((el) => {
+			const cellStatus = el.id.split("_");
+			const row = Number(cellStatus[0]);
+			const col = Number(cellStatus[1]);
+
+			el.className = this.dead;
+			this.currentGenCells[row][col] = 0;
+		});
+
+		if (this.EVOLVING) this.stopEvolve();
+
+		if (this.createBtn.disabled) this.createBtn.disabled = false;
+		if (!this.speedInput.disabled) this.speedInput.disabled = true;
+
+		this.disableStartAndResetBtns();
+	}
+	/* RANDOM CELL GENERATION BEFORE EVOLUTION */
+	createBtnClick() {
+		this.enableStartAndResetBtns();
+
+		// Check if there's already alive cells and kill them if so
+		for (let i = 0; i < this.Rows; i++) {
+			if (this.currentGenCells[i].includes(1)) {
+				this.ALIVE = true;
+
+				if (this.ALIVE) {
+					this.ALIVE = false;
+					this.resetBtnClick();
+					this.enableStartAndResetBtns();
+				}
+			}
+		}
+
+		this.hideIntro();
+
+		// Generate random alive cells
+		const totalCells = this.Cells.length;
+		const maxCells = totalCells * 0.35;
+
+		const uniqueSet = new Set();
+		while (uniqueSet.size < maxCells) {
+			const random = Math.floor(Math.random() * totalCells);
+			uniqueSet.add(random);
+
+			this.Cells.forEach((el, i) => {
+				const cellStatus = el.id.split("_");
+				const row = Number(cellStatus[0]);
+				const col = Number(cellStatus[1]);
+
+				if (i === random) {
+					el.className = this.alive;
+					this.currentGenCells[row][col] = 1;
+				}
+			});
+		}
+	}
+	/* AESTHETICS */
+	gridBtnClick(e) {
+		const btn = e.currentTarget;
+		const container = this.container;
+
+		if (container.dataset.grid === "on") {
+			container.dataset.grid = "off";
+			btn.textContent = "Grid on";
+		} else {
+			container.dataset.grid = "on";
+			btn.textContent = "Grid off";
+		}
+	}
+
+	setModeCSSVars(el, bg, base) {
+		this.root.style.setProperty("--c_bg", bg);
+
+		// If black/white color is/was-last selected, replace color base
+		if (this.controls.dataset.hue === "06")
+			this.root.style.setProperty("--c_base", base);
+	}
+
+	modeBtnClick(e) {
+		const btn = e.currentTarget;
+		const bwColor = document.querySelector('[data-color="06"]');
+
+		const cWhite = "var(--c_06)";
+		const cBlack = "var(--c_07)";
+
+		if (this.dBody.dataset.mode === "dark") {
+			// Light mode
+			this.dBody.dataset.mode = "light";
+			btn.textContent = "Dark mode";
+			bwColor.title = "Black cells";
+
+			this.setModeCSSVars(bwColor, cWhite, cBlack);
+		} else {
+			// Dark mode
+			this.dBody.dataset.mode = "dark";
+			btn.textContent = "Light mode";
+			bwColor.title = "White cells";
+
+			this.setModeCSSVars(bwColor, cBlack, cWhite);
+		}
+	}
+
+	resetAllInputRadios(all) {
+		all.forEach((el) => {
+			const input = el.querySelector("input");
+			input.removeAttribute("checked");
+			input.checked = false;
+		});
+	}
+
+	checkInputRadio(el) {
+		const input = el.querySelector("input");
+		input.setAttribute("checked", "checked");
+		input.checked = true;
+	}
+
+	shapeBtnsClick(e) {
+		this.resetAllInputRadios(this.shapeBtns);
+
+		// Apply input radio selected rules
+		const btn = e.currentTarget;
+		this.checkInputRadio(btn);
+
+		const container = this.container;
+		const gridType = btn.dataset.type;
+		container.dataset.shape = gridType;
+
+		const controls = this.controls;
+		controls.dataset.form = gridType;
+	}
+
+	colorBtnsClick(e) {
+		this.resetAllInputRadios(this.colorBtns);
+
+		// Apply input radio selected rules
+		const btn = e.currentTarget;
+		this.checkInputRadio(btn);
+
+		const colorPrefix = btn.dataset.color;
+
+		// Add data attribute to style cell's emojis
+		if (colorPrefix === "emoji") this.container.dataset.td = "emoji";
+
+		// Add color prefix only if real color
+		if (colorPrefix !== "emoji") {
+			this.container.dataset.td = "color";
+			this.controls.dataset.hue = colorPrefix;
+		}
+
+		if (this.dBody.dataset.mode === "light") {
+			// Light mode
+			if (colorPrefix !== "emoji") {
+				if (colorPrefix === "06") {
+					this.root.style.setProperty("--c_base", "var(--c_07)");
+				} else {
+					this.root.style.setProperty("--c_base", `var(--c_${colorPrefix})`);
+				}
+			}
+		} else {
+			// Dark mode
+			if (colorPrefix !== "emoji")
+				this.root.style.setProperty("--c_base", `var(--c_${colorPrefix})`);
+		}
+	}
+
+	textInputChange(e) {
+		const input = e.currentTarget;
+		const inputVal = input.value;
+
+		this.root.style.setProperty("--c_emoji", `"${inputVal}"`);
+	}
+
+	speedRangeChange(e) {
+		const range = e.currentTarget;
+		const rangeVal = range.querySelector("input").value;
+		const rangeNr = Number(rangeVal);
+
+		this.timerSpeed = rangeNr;
+	}
+	/* RESTART GOL AND RECREATE GRID WHEN RESIZED */
+	resize() {
+		this.resetBtnClick();
+		this.createSetup();
+	}
+
+	addWindowEvents() {
+		window.addEventListener("resize", this.resize.bind(this));
+	}
+	/* ALL CLICK EVENTS */
+	addEvents() {
+		this.createBtn.addEventListener("click", this.createBtnClick.bind(this));
+		this.evolveBtn.addEventListener("click", this.evolveBtnClick.bind(this));
+		this.resetBtn.addEventListener("click", this.resetBtnClick.bind(this));
+		this.gridBtn.addEventListener("click", this.gridBtnClick.bind(this));
+		this.modeBtn.addEventListener("click", this.modeBtnClick.bind(this));
+
+		this.shapeBtns.forEach((el) =>
+			el.addEventListener("click", this.shapeBtnsClick.bind(this))
+		);
+		this.colorBtns.forEach((el) =>
+			el.addEventListener("click", this.colorBtnsClick.bind(this))
+		);
+
+		this.textInput.addEventListener("keyup", this.textInputChange.bind(this));
+		this.speedRange.addEventListener("change", this.speedRangeChange.bind(this));
+	}
 }
 
 const gol = document.querySelector("#gameoflife");
